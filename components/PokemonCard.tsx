047bed2c636f9f935895e53af389a9d7f42db11a
marko-codeasy <marko@codeasy.com>
2021-10-15 12:41:45 +0000

    Pokemon components
    Adds pokemon card and pokemon logo component

---
+++ components/PokemonCard.tsx
@@ -0,0 +1,49 @@
+import { Pokemon } from '../models/pokemon'
+
+import { capitalize } from '../helpers/capitalize'
+
+interface Props extends Pokemon {
+  captured: boolean
+  onCaptured?: (pokemonId: number) => void
+}
+
+export default function PokemonCard({ id, name, types, captured, onCaptured }: Props): JSX.Element | null {
+  return (
+    <div className="w-full bg-white rounded-lg p-4 flex flex-col justify-center items-center shadow">
+      <div className="mb-2">
+        <img
+          className="object-center object-cover rounded-full h-36 w-36"
+          src={`https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${name}.png`}
+          alt="photo"
+        />
+      </div>
+      <div className="text-center mb-4">
+        <p className="text-xl text-gray-700 font-bold mb-2">{capitalize(name)}</p>
+        {types.map((type) => (
+          <span
+            key={type}
+            className="inline-block rounded-full text-gray-600 bg-gray-100 px-2 py-1 text-xss font-bold mr-3"
+          >
+            {type.toUpperCase()}
+          </span>
+        ))}
+      </div>
+      <div>
+        <label className="flex justify-start items-start">
+          <div className="bg-white border-2 rounded border-gray-300 w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-300">
+            <input
+              type="checkbox"
+              checked={captured}
+              className="opacity-0 absolute cursor-pointer"
+              onChange={() => onCaptured && onCaptured(id)}
+            />
+            <svg className="fill-current hidden w-4 h-4 text-blue-300 pointer-events-none" viewBox="0 0 20 20">
+              <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+            </svg>
+          </div>
+          <div className="select-none text-gray-400 cursor-pointer">Captured</div>
+        </label>
+      </div>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,12 @@ import { FormEvent } from 'react'
 import client from '../graphql/client'
 import { GET_POKEMONS } from '../graphql/query/get-pokemons'
 import { Pokemon } from '../models/pokemon'
+import PokemonCard from '../components/PokemonCard'
+import PokemonLogo from '../components/PokemonLogo'
 
 interface HomeProps {
   pokemons: Pokemon[]
 }
-
-const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
 
 const Home: NextPage<HomeProps> = ({ pokemons }: HomeProps) => {
   return (
@@ -20,13 +20,7 @@ const Home: NextPage<HomeProps> = ({ pokemons }: HomeProps) => {
         <meta name="description" content="Poxed using PokeAPI GraphQL" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex justify-center max-w-4xl mx-auto px-4 sm:px-6 lg:px-4 py-4">
-        <img
-          className="object-center object-cover h-24"
-          src="https://www.pinclipart.com/picdir/big/379-3791327_pokemon-logos-png-vector-pokemon-logo-transparent-background.png"
-          alt="logo"
-        />
-      </div>
+      <PokemonLogo />
       <form
         className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-4 py-4"
         onSubmit={(event: FormEvent<HTMLFormElement>) => event.preventDefault()}
@@ -41,40 +35,7 @@ const Home: NextPage<HomeProps> = ({ pokemons }: HomeProps) => {
       <section className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {pokemons.map((pokemon) => (
-            <div
-              key={pokemon.id}
-              className="w-full bg-white rounded-lg p-4 flex flex-col justify-center items-center shadow"
-            >
-              <div className="mb-2">
-                <img
-                  className="object-center object-cover rounded-full h-36 w-36"
-                  src={`https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${pokemon.name}.png`}
-                  alt="photo"
-                />
-              </div>
-              <div className="text-center mb-4">
-                <p className="text-xl text-gray-700 font-bold mb-2">{capitalize(pokemon.name)}</p>
-                {pokemon.types.map((type) => (
-                  <span
-                    key={type}
-                    className="inline-block rounded-full text-gray-600 bg-gray-100 px-2 py-1 text-xss font-bold mr-3"
-                  >
-                    {type.toUpperCase()}
-                  </span>
-                ))}
-              </div>
-              <div>
-                <label className="flex justify-start items-start">
-                  <div className="bg-white border-2 rounded border-gray-300 w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-300">
-                    <input type="checkbox" className="opacity-0 absolute cursor-pointer" />
-                    <svg className="fill-current hidden w-4 h-4 text-blue-300 pointer-events-none" viewBox="0 0 20 20">
-                      <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
-                    </svg>
-                  </div>
-                  <div className="select-none text-gray-400 cursor-pointer">Captured</div>
-                </label>
-              </div>
-            </div>
+            <PokemonCard key={pokemon.id} {...pokemon} captured={false} onCaptured={(id) => alert(id)} />
           ))}
         </div>
       </section>
