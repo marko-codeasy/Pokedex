51eda71e00630917d83f96deed0d7e281832c0db
marko-codeasy <marko@codeasy.com>
2021-10-15 13:34:50 +0000

    Seach pokemons by name

--- models/pokemon.ts
+++ models/pokemon.ts
@@ -1,3 +1,4 @@
+/* TODO: generate graphql types using apollo codegen */
 export interface GetPokemonsQueryData {
   pokemons: {
     id: number
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useLazyQuery } from '@apollo/client'
 
 import client from '../graphql/client'
 import { GET_POKEMONS } from '../graphql/query/get-pokemons'
@@ -9,11 +11,33 @@ import PokemonCard from '../components/PokemonCard'
 import PokemonLogo from '../components/PokemonLogo'
 import SearchBar from '../components/SearchBar'
 
+const toPokemon = ({ id, name, stats }: GetPokemonsQueryData['pokemons'][0]) => ({
+  id,
+  name: name,
+  types: stats.nodes[0].types.map((type) => type.type.name),
+})
+
 interface Props {
   pokemons: Pokemon[]
 }
 
-const Home: NextPage<Props> = ({ pokemons }: Props) => {
+const Home: NextPage<Props> = ({ pokemons: initialPokemons }: Props) => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>(initialPokemons)
+  const [getPokemons, { loading }] = useLazyQuery<GetPokemonsQueryData>(GET_POKEMONS, {
+    variables: {
+      orderBy: {
+        id: 'asc',
+      },
+    },
+    onCompleted: (data) => {
+      setPokemons(data.pokemons.map(toPokemon))
+    },
+  })
+
+  const handleSearch = (searchTerm: string) => {
+    getPokemons({ variables: { limit: 12, offset: 0, where: { name: { _regex: `^${searchTerm}` } } } })
+  }
+
   return (
     <div className="min-h-screen bg-blue-200">
       <Head>
@@ -22,8 +46,8 @@ const Home: NextPage<Props> = ({ pokemons }: Props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <PokemonLogo />
-      <SearchBar onSearch={(searchTerm) => alert(searchTerm)} />
-      <section className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+      <SearchBar onSearch={handleSearch} />
+      <section className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-4 py-12" style={{ opacity: loading ? 0.5 : 1 }}>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {pokemons.map((pokemon) => (
             <PokemonCard key={pokemon.id} {...pokemon} captured={false} onCaptured={(id) => alert(id)} />
@@ -46,11 +70,7 @@ export async function getServerSideProps() {
     },
   })
 
-  const pokemons = data.pokemons.map(({ id, name, stats }) => ({
-    id,
-    name: name,
-    types: stats.nodes[0].types.map((type: any) => type.type.name),
-  }))
+  const pokemons = data.pokemons.map(toPokemon)
 
   return {
     props: {
