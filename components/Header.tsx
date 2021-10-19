708c9b3578d62496b58dcf70508f5b59956a4e4e
marko-codeasy <marko@codeasy.com>
2021-10-19 09:38:58 +0000

    Implement Header component
    - Added dropdown to select current user
    - Hardcoding 3 dummy users

---
+++ components/Header.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+
+interface User {
+  id: number
+  firstName: string
+  lastName: string
+}
+
+const dummyUsers: User[] = [
+  { id: 1, firstName: 'John', lastName: 'Doe' },
+  { id: 2, firstName: 'Jane', lastName: 'Doe' },
+  { id: 3, firstName: 'Will', lastName: 'Smith' },
+]
+
+export default function Header(): JSX.Element | null {
+  const [selectedUserId, setSelectedUserId] = useState<number>(1)
+
+  useEffect(() => {
+    const storedCurrentUserString = localStorage.getItem('currentUser')
+    if (!storedCurrentUserString) {
+      return
+    }
+
+    const storedCurrentUser: User = JSON.parse(storedCurrentUserString)
+    setSelectedUserId(storedCurrentUser.id)
+  }, [])
+
+  const handleSelectedUser = (event: ChangeEvent<HTMLSelectElement>) => {
+    const id = Number(event.target.value)
+    setSelectedUserId(id)
+
+    const user = dummyUsers.find((user) => user.id === id)
+    localStorage.setItem('currentUser', JSON.stringify(user))
+  }
+
+  return (
+    <div className="absolute right-0 mr-2 mt-2">
+      <select
+        className="text-base text-gray-800 outline-none border-2 px-4 py-2 rounded-lg mr-2"
+        value={selectedUserId}
+        onChange={handleSelectedUser}
+      >
+        {dummyUsers.map(({ id, firstName, lastName }) => (
+          <option key={id} value={id}>
+            {firstName} {lastName}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+import Header from '../components/Header'
 
 import client from '../graphql/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <Header />
+
       <Component {...pageProps} />
     </ApolloProvider>
   )
