19de779ecd23f0a167df0a400eaa10c684436d0b
marko-codeasy <marko@codeasy.com>
2021-10-19 11:35:59 +0000

    Add click handler to logo for navigation on Home page

--- components/PokemonLogo.tsx
+++ components/PokemonLogo.tsx
@@ -1,10 +1,19 @@
+import { useRouter } from 'next/router'
+
 export default function PokemonLogo(): JSX.Element {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('/')
+  }
+
   return (
     <div className="flex justify-center">
       <img
-        className="object-center object-cover h-24"
+        className="object-center object-cover h-24 cursor-pointer"
         src="https://www.pinclipart.com/picdir/big/379-3791327_pokemon-logos-png-vector-pokemon-logo-transparent-background.png"
         alt="logo"
+        onClick={handleClick}
       />
     </div>
   )
