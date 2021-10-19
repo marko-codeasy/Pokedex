34a18fdcc51c018e6ef1e6e9b4a37f87a4928c87
marko-codeasy <marko@codeasy.com>
2021-10-19 14:23:26 +0000

    Add slight animation to paginator buttons

--- components/Paginator.tsx
+++ components/Paginator.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion'
+
 interface Props {
   page: number
   perPage: number
@@ -7,21 +9,23 @@ interface Props {
 
 export default function Paginator({ page, perPage, total, onPageChange }: Props): JSX.Element | null {
   return (
-    <div className="inline-flex mt-2 mb-2 xs:mt-0">
-      <button
+    <div className="inline-flex mt-2 mb-6 xs:mt-0">
+      <motion.button
         disabled={page === 1}
         className="text-sm bg-white hover:bg-blue-300 text-gray-800 font-semibold py-2 px-4 rounded-l mr-1 disabled:opacity-50"
         onClick={() => onPageChange(Math.max(--page, 1))}
+        whileTap={{ scale: 0.9 }}
       >
         Prev
-      </button>
-      <button
+      </motion.button>
+      <motion.button
         disabled={page * perPage >= total}
         className="text-sm bg-white hover:bg-blue-300 text-gray-800 font-semibold py-2 px-4 rounded-r disabled:opacity-50"
         onClick={() => onPageChange(Math.min(++page, Math.ceil(total / perPage)))}
+        whileTap={{ scale: 0.9 }}
       >
         Next
-      </button>
+      </motion.button>
     </div>
   )
 }
